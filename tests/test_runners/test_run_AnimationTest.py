984d24bacbade408090426b0a912b924cb3c77b5
DoSmile0705 <dev.lifecreator@gmail.com>
2024-11-23 00:07:37 +0900

    Updated and moved generate_test_ruynners.py

--- tests/test_runners/test_run_AnimationTest.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import tests.test_providers.sample_implemented as sample_implemented
-
-try:
-    unittest.main(argv=["-v", "sample_implemented.AnimationTest"], exit=False)
-except Exception as e:
-    print(e)
--- tests/test_runners/test_run_WireTest.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import tests.test_providers.sample_implemented as sample_implemented
-
-try:
-    unittest.main(argv=["-v", "sample_implemented.WireTest"], exit=False)
-except Exception as e:
-    print(e)
