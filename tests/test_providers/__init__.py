0d66b6845bf1a514b8c06c418cb09c2f697cf26c
Shehab Attia <shehabattia96@gmail.com>
2024-11-24 23:44:54 -0500

    Blender documents are cleared between test runs

--- tests/test_providers/__init__.py
+++ tests/test_providers/__init__.py
@@ -5,10 +5,27 @@ from codetocad import *
 from codetocad.codetocad_types import *
 
 
+def _clear_document():
+    """
+    This is temporary, we need to replace this with some native method like undo
+    or clear document.
+    In the meantime, we only have the Blender integration to worry about,
+    so this will be hard-coded here:
+    """
+    try:
+        import bpy
+
+        bpy.ops.wm.revert_mainfile()
+    except:
+        ...
+
+
 class TestProviderCase(unittest.TestCase):
 
     def setUp(self) -> None:
         print(f"\nRunning test: {self._testMethodName}")
+
+        _clear_document()
 
         # Check if the interface is already injected into providers
         # otherwise, use the sample provider
